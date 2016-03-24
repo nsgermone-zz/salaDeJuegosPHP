f7a2a7f0acd2a4617f69b0c61a7164db3aa1e2d5
Nicolás Germone <nsgermone@gmail.com>
2016-03-24 10:21:47 -0300

    Added files via upload

---
+++ ingreso.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login de Usuario</title>
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" type="text/css" href="CSS/estilo.css">
+	<link rel="stylesheet" type="text/css" href="CSS/animacion.css">
+
+</head>
+<body>
+	<?php
+
+	?>
+</body>
+</html>
